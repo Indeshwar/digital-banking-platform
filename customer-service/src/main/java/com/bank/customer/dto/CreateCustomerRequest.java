4cbb4a2e19b552e36905d7c70a9533c0f9336f66
Indeshwar Chaudhary <indeshwar@Indeshwars-MacBook-Air-2.local>
2026-02-20 12:19:14 -0600

    Added Validation

--- customer-service/src/main/java/com/bank/customer/dto/CreateCustomerRequest.java
+++ customer-service/src/main/java/com/bank/customer/dto/CreateCustomerRequest.java
@@ -1,5 +1,9 @@
 package com.bank.customer.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
@@ -8,13 +12,20 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 public class CreateCustomerRequest {
+    @NotBlank(message = "Please enter first name")
     private String firstName;
 
+    @NotBlank(message = "Please enter last name")
     private String lastName;
 
+    @Email(message = "Please enter valid email")
     private String email;
 
+    private static final String PHONE_REGEX = "^(\\([0-9]{3}\\)|[0-9]{3}-)[0-9]{3}-[0-9]{4}$";
+
+    @Pattern(regexp = PHONE_REGEX, message = "Invalid phone number format. Expected formats: 123-456-7890 or (123)456-7890")
     private String phone;
 
+    @Valid
     private AddressDto address;
 }
